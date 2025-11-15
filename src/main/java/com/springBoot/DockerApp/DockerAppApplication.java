bf3058cb58c8e6a4c6eede5f935ec5b96c685916
saravananraj <saravananapec@gmail.com>
2025-11-15 19:38:31 +0530

    First Commit

---
+++ src/main/java/com/springBoot/DockerApp/DockerAppApplication.java
@@ -0,0 +1,21 @@
+package com.springBoot.DockerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerAppApplication {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to Docker deploy using Google Jib";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBoot/DockerApp/DockerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.DockerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
